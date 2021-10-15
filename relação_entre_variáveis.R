54cdcbf8b83cf871866db3f3e7baa260553675e4
Mauritia-flexuosa <euviburiti@gmail.com>
2021-10-15 15:42:08 -0300

    arrumei o Readme

--- relação_entre_variáveis.R
+++ relação_entre_variáveis.R
@@ -1,74 +1,3 @@
-library(tidyverse)
-library(patchwork)
-library(RColorBrewer)
-
-fire <- read.table("./fire_freq_pontos.txt")
-
-tc_field <- read.table("./tc_field.txt", h = T)
-#write.table(tc_field, "tc_field.txt")
-
-tc_hansen <- read.table("./tc_hansen.txt", h = T) 
-#write.table(tc_hansen, "tc_hansen.txt")
-
-
-coup <- read.table("./coup.txt", h = T) 
-#write.table(coup, "coup.txt")
-
-traits <- read.table("./traits_community_mean.txt", h = T)
-#write.table(traits, "traits.txt")
-
-
-evi2 <- read.table("evi2_estatistic.txt", h = T)
-
-deciduousness <- (tc_field$wet-tc_field$dry)
-data <- coup %>% add_column(freq_fire = fire[1:30,1]) %>% add_column(tree_cover_wet = tc_field$wet)%>% add_column(tree_cover_dry = tc_field$dry) %>% add_column(tree_cover_hansen = tc_hansen[1:30,1]) %>% add_column(phenology = deciduousness) %>% add_column(evi2[1:30,1:4])
-
-
-a<- data %>% 
-  ggplot()+
-   aes(y = Coup_lag_max, fill = factor(Vegetation_type), show.legend = FALSE)+
-  geom_boxplot()+
-  ggtitle("Maximum coupling")+
-  scale_fill_manual(c("purple","grey","orange"))+
-  ylab("Coupling between EVI2 and precipitation")+
-  xlab("Vegetation type")
-
-
-b<-data %>% 
-  ggplot()+
-  aes(y = Lag, fill = factor(Vegetation_type), show.legend = FALSE)+
-  geom_boxplot()+
-  ggtitle("Lag in months for the maximum coupling")+
-  scale_fill_manual(c("purple","grey","orange"))+
-  ylab("Lag in months")+
-  xlab("Vegetation type")
-
-
-c<-data %>% 
-  ggplot()+
-  aes(y = deciduousness, fill = factor(Vegetation_type), show.legend = FALSE)+
-  geom_boxplot()+
-  ggtitle("Deciduousness", subtitle = "Changes in tree cover among dry and wet seasons")+
-  scale_fill_manual(c("purple","grey","orange"))+
-  ylab("Deciduousness")+
-  xlab("Vegetation type")
-
-d <- data %>% 
-  ggplot()+
-  aes(y = tree_cover_wet, fill = factor(Vegetation_type), show.legend = FALSE)+
-  geom_boxplot()+
-  ggtitle("Tree cover", subtitle = "densiometer")+
-  scale_fill_manual(c("purple","grey","orange"))+
-  ylab("Tree cover (%)")+
-  xlab("Vegetation type")
-
-
-(a|b)/(c|d)
-
-
-relative_bark_hickness <- (traits$Bark_thickness/traits$cwm_basal_area)
-
-a1 
 
 
 library(tidyverse)
@@ -94,11 +23,13 @@ traits <- read.table("./traits_community_mean.txt", h = T)
 evi2 <- read.table("evi2_estatistic.txt", h = T)
 
 deciduousness <- (tc_field$wet-tc_field$dry)
+
+#relative_bark_hickness <- (traits$Bark_thickness/traits$cwm_basal_area)
 
 data <- coup %>% add_column(freq_fire = fire[1:30,1]) %>% add_column(tree_cover_wet = tc_field$wet)%>% add_column(tree_cover_dry = tc_field$dry) %>% add_column(tree_cover_hansen = tc_hansen[1:30,1]) %>% add_column(phenology = deciduousness) %>% add_column(evi2[1:30,1:4]) %>% rename(Vegetation_type=Fitofisionomia)
 
 
-data %>% summary %>% knitr::kable()
+data %>% knitr::kable() %>% view
 
 a<- data %>% 
   ggplot()+
@@ -129,8 +60,9 @@ c<-data %>%
   xlab("Vegetation type")
 
 
-
+png("state_var.png")
 (a|b|c)
+dev.off()
 
 a3<-data %>% 
   ggplot()+
@@ -169,9 +101,9 @@ d3<-data %>%
   xlab("Vegetation type")
 
 
-
+png("EVI2_vars.png")
 (a3|b3)/(c3|d3)
-
+dev.off()
 
 a4 <- data %>%  ggplot()+
   aes(x= tree_cover_wet, fill = factor(Vegetation_type))+
@@ -189,47 +121,46 @@ b4 <- data %>% ggplot()+
   ylab("Tree cover (%)")+
   xlab("Vegetation type")
 
-
+png("tree_covers.png")
 a4|b4
-
+dev.off()
 
 a5 <- traits %>% 
   ggplot()+
-  aes(y = Tree_height, fill = factor(Vegetation_type), show.legend = FALSE)+
-  geom_boxplot()+
+  aes(y = Tree_height, fill = factor(Vegetation_type), x = factor(Vegetation_type))+
+  geom_boxplot(show.legend = FALSE)+
   ggtitle("Tree height")+
   scale_fill_manual(values=c("purple", "darkgrey", "orange2"))+
   ylab("Tree height (m)")+
   xlab("Vegetation type")
 
-b5 <- traits %>% 
+b5 <- traits %>%
   ggplot()+
-  aes(y = basal_area_total, fill = factor(Vegetation_type), show.legend = FALSE)+
-  geom_boxplot()+
+  aes(y = basal_area_total, fill = factor(Vegetation_type), x = factor(Vegetation_type))+
+  geom_boxplot(show.legend = FALSE)+
   ggtitle("Basal area total")+
   scale_fill_manual(values=c("purple", "darkgrey", "orange2"))+
   ylab("Basal area total")+
   xlab("Vegetation type")
 
-c5 <- data %>% 
+c5 <- data %>% mutate(Vegetation_type=traits$Vegetation_type) %>% 
   ggplot()+
-  aes(y = tree_cover_wet, fill = factor(Vegetation_type), show.legend = FALSE)+
-  geom_boxplot()+
+  aes(y = tree_cover_wet, fill = factor(Vegetation_type), x = factor(Vegetation_type))+
+  geom_boxplot(show.legend = FALSE)+
   ggtitle("Tree cover (densiometer)")+
   scale_fill_manual(values=c("purple", "darkgrey", "orange2"))+
   ylab("Tree cover")+
   xlab("Vegetation type")
 
 
-d5 <- data %>% 
+d5 <- data %>% mutate(Vegetation_type=traits$Vegetation_type) %>% 
   ggplot()+
-  aes(y = freq_fire, fill = factor(Vegetation_type), show.legend = FALSE)+
-  geom_boxplot()+
+  aes(y = freq_fire, fill = factor(Vegetation_type), x = factor(Vegetation_type))+
+  geom_boxplot(show.legend = FALSE)+
   ggtitle("Frequência de fogo", subtitle = "Período de 1985-2018")+
-  scale_fill_manual(c("purple","grey","orange"))+
-  ylab("Times burned")+
+  scale_fill_manual(values=c("purple", "darkgrey", "orange2"))+  ylab("Times burned")+
   xlab("Vegetation type")
 
-pdf("./traits_e_fogo.pdf", family = "sans")
+png("./traits_e_fogo.png")
 (a5|b5)/(c5|d5)
 dev.off()
